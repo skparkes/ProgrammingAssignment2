99c7893343edf801c86835c69ae94dd7ac742ea3
skparkes <bias.variance@gmail.com>
2014-07-12 16:09:44 -0400

    Write helpful comments for the anticipated functions.
    Do this because it is good practice and it was requested.

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,16 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions are designed to cache a Matrix Inversion result.
+## This is done because Solving (i.e. inverting) a Matrix can be expensive
 
-## Write a short comment describing this function
+## This function generates an enhanced "Matrix Object" that has embedded methods (i.e. functions)
+##   that (get/set) the value of the (matrix/inverse).
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## This function returns the Inverse of an enhanced "Matrix Object".
+##   It does so efficiently by using the available get/set methods.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
